a76092d05af98ecf5face4339cd3f8d1bdffb8b3
dılo sürücü <berxudar@gmail.com>
2021-07-14 18:03:44 +0300

    ignore comment lines when read show path repo

--- show.repo.path.sh
+++ show.repo.path.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-path=$(cat <"/etc/elasticsearch/elasticsearch.yml" | grep "path.repo" | awk '{print $2}')
+if [ ! "$SUDO_USER" ]; then
+  error_text "This option requires root privileges."
+  echo ""
+  exit
+fi
+path=$(cat <"/etc/elasticsearch/elasticsearch.yml" | grep "path.repo" | grep -v "#"| awk '{print $2}')
 green_text "$path"
