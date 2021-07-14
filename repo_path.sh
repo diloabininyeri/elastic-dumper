2f976e8f615a2df69ce3100cd2f456df58c710e3
dılo sürücü <berxudar@gmail.com>
2021-07-14 17:52:26 +0300

    If there is a comment line, it was processed as ignore

--- repo_path.sh
+++ repo_path.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-file="/etc/elasticsearch/elasticsearch.yml"
+#!/bin/bash
+source "color.sh"
 
-if ! grep -q path.repo "$file"; then
+if [ ! "$SUDO_USER" ]; then
+  error_text "This script requires root privileges."
+  echo ""
+  exit
+fi
+file="/etc/elasticsearch/elasticsearch.yml"
+check=$(grep -n path.repo "$file" | grep -v "#")
+
+if [ ${#check} -eq 0 ]; then
   echo "path.repo: /home/my_fs_backup_location" >>$file
   green_text "path.repo wrote to elasticsearch.yml file"
   exit
 fi
-
 
 info_text "already written "
 echo ""
--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source "color.sh"
-
-error_text "erororor"
-green_text_bold "dddd"
-echo "dddd"
-colorize red "merhababa"
-colorize  yellow "mahmut"
